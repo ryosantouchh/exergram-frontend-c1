06fb9a0c37e46d0846b19580dbb4c30689898c40
ryosantouchh <kittitat.suntimak@gmail.com>
2023-04-22 01:48:16 +0700

    70 percent dashboard with pull and merge in local repo

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import "../styles/Dashboard.css";
-import "../styles/Dashboard.css";
 import Layout from "../layout/Layout";
 import SummaryTrack from "../components/Dashboard/SummaryTrack";
 import DashboardRight from "../components/Dashboard/DashboardRight";
