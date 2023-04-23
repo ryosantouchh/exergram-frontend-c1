be20a6e5c7322ed36fd38285bb729f26b6b4ac96
TanapornSrisawan <125846576+TanapornSrisawan@users.noreply.github.com>
2023-04-23 17:26:10 +0700

    create landing page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import Layout from "../layout/Layout";
-import ActivityPage from "./ActivityPage"
+// import ActivityPage from "./ActivityPage"
 import Footer from "../components/Footer/Footer"
+import LandingPage from "../pages/LandingPage"
 
 
 function Home() {
   return (
     <Layout>
-      <ActivityPage />
+      <LandingPage />
+      {/* <ActivityPage /> */}
     </Layout>
   );
 }
---
+++ src/pages/LandingPage.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+import "../styles/LandingPage.css"
+import Layout from "../layout/Layout";
+
+const LandingPage = () => {
+    return (
+        <>
+            <div className="hero-container-fluid">
+                <div className="hero-container">
+                    <div className="hero-left">
+                        <div className="img">
+                            <img src="../assets/images/runnerSVG.svg" alt="" />
+                        </div>
+
+                        <div className="bg-img">
+                            <div className="bg-img-r"></div>
+                            <div className="bg-img-m"></div>
+                            <div className="bg-img-l"></div>
+                        </div>
+                    </div>
+
+                    <div className="hero-right">
+                        <button className="orange-trans-btn">Join Now</button>
+                    </div>
+                </div>
+
+            </div>
+
+
+            <div className="info-container-fluid">
+                <div className="info-container">
+                    <div className="info-left">
+                        <h2>SET GOALS.</h2>
+                        <h2>LOG WORKOUTS.</h2>
+                        <h2>STAY ON TRACK.</h2>
+                        <p>
+                            Easily track your Workouts, set <br />
+                            Training Plans, and discover new <br />
+                            Workout Routines to crush your goals.
+                        </p>
+                        <button className="orange-btn">GET START</button>
+                    </div>
+                    <div className="info-right">
+                        <img src="https://images.unsplash.com/photo-1474546652694-a33dd8161d66?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1484&q=80"
+                            alt="" />
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default LandingPage;
