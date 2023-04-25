6c919102f10e250cc4694f7e3eb99be422cb508e
TanapornSrisawan <125846576+TanapornSrisawan@users.noreply.github.com>
2023-04-25 22:12:43 +0700

    update img

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import Layout from "../layout/Layout";
-// import ActivityPage from "./ActivityPage"
 import Footer from "../components/Footer/Footer"
 import LandingPage from "../pages/LandingPage"
 
@@ -9,7 +8,6 @@ function Home() {
   return (
     <Layout>
       <LandingPage />
-      {/* <ActivityPage /> */}
     </Layout>
   );
 }
